afdd2d7b7592ad646b4b9f6f5676918261986866
Anthony J. Ruffa <ajosephruffa@gmail.com>
2016-02-15 14:49:06 -0500

    removed outdated back end java code

--- WhiskyBarrl/app/src/main/java/edu/wpi/mis270xteam1/whiskybarrl/MainActivity.java
+++ WhiskyBarrl/app/src/main/java/edu/wpi/mis270xteam1/whiskybarrl/MainActivity.java
@@ -10,22 +10,10 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button newWhiskeyButton;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        newWhiskeyButton = (Button) findViewById(R.id.new_whiskey);
-
-        newWhiskeyButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, NewWhiskeyActivity.class);
-                startActivity(i);
-            }
-        });
     }
 
     @Override
