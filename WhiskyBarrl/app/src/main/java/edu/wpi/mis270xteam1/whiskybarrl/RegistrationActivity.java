e3afdf16761213de4bf158592aba0231eeaef729
Anthony J. Ruffa <ajosephruffa@gmail.com>
2016-02-16 20:30:02 -0500

    added more validation for user registration

--- WhiskyBarrl/app/src/main/java/edu/wpi/mis270xteam1/whiskybarrl/RegistrationActivity.java
+++ WhiskyBarrl/app/src/main/java/edu/wpi/mis270xteam1/whiskybarrl/RegistrationActivity.java
@@ -9,6 +9,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.List;
+
 public class RegistrationActivity extends Activity {
 
     private EditText editTextUsername;
@@ -51,18 +53,11 @@ public class RegistrationActivity extends Activity {
             public void onClick(View v) {
                 if (isRegistrationInfoValid()) {
                     User user = createNewUser();
-
                     dbHandler.addUser(user);
-
-                    Toast.makeText(
-                            RegistrationActivity.this,
-                            "Account successfully registered.",
-                            Toast.LENGTH_SHORT).show();
+                    Toast.makeText(RegistrationActivity.this, "Account successfully registered.", Toast.LENGTH_SHORT).show();
                 } else {
-                    Toast.makeText(
-                            RegistrationActivity.this,
-                            "Unable to create a user with the given information.",
-                            Toast.LENGTH_SHORT).show();
+                    String errorMessage = getRegistrationErrorMessage();
+                    Toast.makeText(RegistrationActivity.this, errorMessage, Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -94,51 +89,74 @@ public class RegistrationActivity extends Activity {
     }
 
     private boolean isRegistrationInfoValid() {
+        return allFieldsNotEmpty() && isUserNameValid()
+                && isPasswordValid() && isAgeValid()
+                && isEmailValid() && isPhoneNumberValid();
+    }
+
+    private boolean allFieldsNotEmpty() {
+        String usernameEntry = editTextUsername.getText().toString();
         String firstNameEntry = editTextFirstName.getText().toString();
         String lastNameEntry = editTextLastName.getText().toString();
         String genderEntry = editTextGender.getText().toString();
         String countryEntry = editTextCountry.getText().toString();
+        String ageEntry = editTextAge.getText().toString();
+        String emailEntry = editTextEmail.getText().toString();
+        String phoneNumberEntry = editTextPhoneNumber.getText().toString();
 
-        return isUserNameValid() && isPasswordValid() && isAgeValid()
-                && isEmailValid() && isPhoneNumberValid()
+        return !TextUtils.isEmpty(usernameEntry)
                 && !TextUtils.isEmpty(firstNameEntry)
                 && !TextUtils.isEmpty(lastNameEntry)
                 && !TextUtils.isEmpty(genderEntry)
-                && !TextUtils.isEmpty(countryEntry);
+                && !TextUtils.isEmpty(countryEntry)
+                && !TextUtils.isEmpty(ageEntry)
+                && !TextUtils.isEmpty(emailEntry)
+                && !TextUtils.isEmpty(phoneNumberEntry);
     }
 
     private boolean isUserNameValid() {
-        String usernameEntry = editTextUsername.getText().toString();
-        User userWithUsername = dbHandler.getUser(usernameEntry);
-
-        return userWithUsername == null && !TextUtils.isEmpty(usernameEntry);
+        return dbHandler.getUser(editTextUsername.getText().toString()) == null;
     }
 
     private boolean isAgeValid() {
-        String ageEntry = editTextAge.getText().toString();
-        return Integer.parseInt(ageEntry) >= 21
-                && !TextUtils.isEmpty(ageEntry);
+        try {
+            return Integer.parseInt(editTextAge.getText().toString()) >= 21;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 
     private boolean isPasswordValid() {
         String initialPWEntry = editTextPassword.getText().toString();
         String reEnterPWEntry = editTextReEnterPassword.getText().toString();
 
-        return !TextUtils.isEmpty(initialPWEntry)
-                && !TextUtils.isEmpty(reEnterPWEntry)
-                && initialPWEntry.equals(reEnterPWEntry);
+        return initialPWEntry.equals(reEnterPWEntry);
     }
 
     private boolean isEmailValid() {
         String emailEntry = editTextEmail.getText().toString();
-        return Patterns.EMAIL_ADDRESS.matcher(emailEntry).matches()
-                && !TextUtils.isEmpty(emailEntry);
+        return Patterns.EMAIL_ADDRESS.matcher(emailEntry).matches();
     }
 
     private boolean isPhoneNumberValid() {
         String phoneNumberEntry = editTextPhoneNumber.getText().toString();
-        return Patterns.PHONE.matcher(phoneNumberEntry).matches()
-                && !TextUtils.isEmpty(phoneNumberEntry);
+        return Patterns.PHONE.matcher(phoneNumberEntry).matches();
     }
 
+    private String getRegistrationErrorMessage() {
+        if (!allFieldsNotEmpty()) {
+            return "One or more fields are blank.";
+        } else if (!isUserNameValid()) {
+            return "Username is already taken.";
+        } else if (!isAgeValid()) {
+            return "You must be at least 21 years of age to use this app.";
+        } else if (!isPasswordValid()) {
+            return "Passwords do not match.";
+        } else if (!isEmailValid()) {
+            return "You must enter a valid email address.";
+        } else if (!isPhoneNumberValid()) {
+            return "You must enter a valid phone number.";
+        }
+        return "";
+    }
 }
