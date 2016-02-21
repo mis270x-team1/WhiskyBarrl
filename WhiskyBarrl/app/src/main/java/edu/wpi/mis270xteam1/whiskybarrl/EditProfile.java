02403af92b6e9a62bcb4c5f3523c8f99bd74d591
Vincent Doyle <Vinnie@Vincents-MacBook-Air.local>
2016-02-21 18:07:38 -0500

    edit profile

---
+++ WhiskyBarrl/app/src/main/java/edu/wpi/mis270xteam1/whiskybarrl/EditProfile.java
@@ -0,0 +1,13 @@
+package edu.wpi.mis270xteam1.whiskybarrl;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EditProfile extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+    }
+}
