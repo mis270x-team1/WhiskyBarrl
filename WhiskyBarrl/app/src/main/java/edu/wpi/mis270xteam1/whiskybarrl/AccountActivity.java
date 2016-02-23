b27ca457e13ad800ecd546d1f318daa152d766d0
Anthony J. Ruffa <ajosephruffa@gmail.com>
2016-02-23 17:26:06 -0500

    changed AccountActivity to extend AppCompatActivity

--- WhiskyBarrl/app/src/main/java/edu/wpi/mis270xteam1/whiskybarrl/AccountActivity.java
+++ WhiskyBarrl/app/src/main/java/edu/wpi/mis270xteam1/whiskybarrl/AccountActivity.java
@@ -1,13 +1,13 @@
 package edu.wpi.mis270xteam1.whiskybarrl;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class AccountActivity extends Activity {
+public class AccountActivity extends AppCompatActivity {
     String currentUsername;
     User currentUser;
     DatabaseHandler db;
